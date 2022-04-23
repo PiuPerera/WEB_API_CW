f620f26443026a3e03f54615459f284bb09eb50b
Udara Sachinthana <u4dara@gmail.com>
2022-04-23 16:34:38 +0530

    Frontend connected

--- CattyLove API/app.js
+++ CattyLove API/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
 const home = require("./routes/home");
 const cats = require("./routes/cats");
 const CattyLove = require("./routes/adminCattyLove");
@@ -11,6 +12,7 @@ mongoose.connect("mongodb://localhost/cattylovedb")
 
 const Port = 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use('/', home);
 app.use('/api/cats', cats);
