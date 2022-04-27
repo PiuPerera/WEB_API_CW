39d1a7431f0e48bef821c76ad5ab3df1e491b704
Udara Sachinthana <u4dara@gmail.com>
2022-04-27 15:46:58 +0530

    env variables created

--- CattyLove API/app.js
+++ CattyLove API/app.js
@@ -2,17 +2,16 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
+const dotenv = require("dotenv").config()
 const home = require("./routes/home");
 const cats = require("./routes/cats");
 const CattyLove = require("./routes/adminCattyLove");
 const auth = require("./routes/auth");
 const users = require("./routes/users");
 
-mongoose.connect("mongodb://localhost/cattylovedb")
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("Connected to db Successfully..."))
 .catch((err) => console.log("Error connecting to the db :", err));
-
-const Port = 4000;
 
 app.use(cors());
 app.use(express.json());
@@ -22,6 +21,6 @@ app.use("/api/cattylove",CattyLove);
 app.use("/api/auth", auth);
 app.use("/api/users", users);
 
-app.listen(Port, () => {
-    console.log("Started Listning on Port :" + Port);
+app.listen(process.env.PORT, () => {
+    console.log("Started Listning on Port :" + process.env.PORT);
 });
---
+++ react-app/src/Components/footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Footer extends Component{
+    state = {}
+
+    render() {
+        return (
+            <React.Fragment>
+                
+            </React.Fragment>
+        )
+    }
+}
